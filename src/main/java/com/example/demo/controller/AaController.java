e12b1ac9fceb4bf881617fea67d1672726ba4704
yujecheon <yujecheon@gmail.com>
2024-11-15 13:59:34 +0900

    신규 파일

---
+++ src/main/java/com/example/demo/controller/AaController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AaController {
+
+    @GetMapping("/api/v1/user")
+    public String user() {
+    	return "82265615";
+    }
+
+}
